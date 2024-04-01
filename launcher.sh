9cabbd73dac8d5d491397256dcc904f512be777c
wweijtje <85878088+wweijtje@users.noreply.github.com>
2024-04-01 13:42:33 +0200

    Add laucher shell script

---
+++ launcher.sh
@@ -0,0 +1,4 @@
+cd ~ || exit # Go to root, if failed kill the script
+source onkyo/bin/activate # Launch the dedicated onkyo environment
+cd  onkyo-api || exit # Go to the proper folder, if not present kill the script
+python app.py # run the app
